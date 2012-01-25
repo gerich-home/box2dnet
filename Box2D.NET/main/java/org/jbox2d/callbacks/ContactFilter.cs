31ae96e6a371f98fb38f20d90f34c253bd0de536
Sergey Gerasimov <gerich.home@gmail.com>
2012-01-25 10:04:51 +0400

    ContactFilter.cs reworked

--- Box2D.NET/main/java/org/jbox2d/callbacks/ContactFilter.cs
+++ Box2D.NET/main/java/org/jbox2d/callbacks/ContactFilter.cs
@@ -1,63 +1,65 @@
-/// <summary>****************************************************************************
-/// Copyright (c) 2011, Daniel Murphy
-/// All rights reserved.
-/// 
-/// Redistribution and use in source and binary forms, with or without modification,
-/// are permitted provided that the following conditions are met:
-/// * Redistributions of source code must retain the above copyright notice,
-/// this list of conditions and the following disclaimer.
-/// * Redistributions in binary form must reproduce the above copyright notice,
-/// this list of conditions and the following disclaimer in the documentation
-/// and/or other materials provided with the distribution.
-/// 
-/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-/// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-/// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-/// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-/// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-/// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-/// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-/// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-/// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-/// POSSIBILITY OF SUCH DAMAGE.
-/// ****************************************************************************
-/// </summary>
-/// <summary> Created at 4:25:42 AM Jul 15, 2010</summary>
+// ****************************************************************************
+// Copyright (c) 2011, Daniel Murphy
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// ****************************************************************************
+
+// Created at 4:25:42 AM Jul 15, 2010
 using System;
 using Filter = org.jbox2d.dynamics.Filter;
 using Fixture = org.jbox2d.dynamics.Fixture;
+
 namespace org.jbox2d.callbacks
 {
-	
-	// updated to rev 100
-	/// <summary> Implement this class to provide collision filtering. In other words, you can implement
-	/// this class if you want finer control over contact creation.
-	/// </summary>
-	/// <author>  Daniel Murphy
-	/// </author>
-	public class ContactFilter
-	{
-		
-		/// <summary> Return true if contact calculations should be performed between these two shapes.</summary>
-		/// <warning>  for performance reasons this is only called when the AABBs begin to overlap. </warning>
-		/// <param name="fixtureA">
-		/// </param>
-		/// <param name="fixtureB">
-		/// </param>
-		/// <returns>
-		/// </returns>
-		public virtual bool shouldCollide(Fixture fixtureA, Fixture fixtureB)
-		{
-			Filter filterA = fixtureA.FilterData;
-			Filter filterB = fixtureB.FilterData;
-			
-			if (filterA.groupIndex == filterB.groupIndex && filterA.groupIndex != 0)
-			{
-				return filterA.groupIndex > 0;
-			}
-			
-			bool collide = (filterA.maskBits & filterB.categoryBits) != 0 && (filterA.categoryBits & filterB.maskBits) != 0;
-			return collide;
-		}
-	}
+
+    // updated to rev 100
+    /// <summary>
+    /// Implement this class to provide collision filtering. In other words, you can implement
+    /// this class if you want finer control over contact creation.
+    /// </summary>
+    /// <author>Daniel Murphy</author>
+    public class ContactFilter
+    {
+
+        /// <summary>
+        /// Return true if contact calculations should be performed between these two shapes.
+        /// </summary>
+        /// <warning>
+        /// for performance reasons this is only called when the AABBs begin to overlap.
+        /// </warning>
+        /// <param name="fixtureA"></param>
+        /// <param name="fixtureB"></param>
+        /// <returns></returns>
+        public virtual bool shouldCollide(Fixture fixtureA, Fixture fixtureB)
+        {
+            Filter filterA = fixtureA.FilterData;
+            Filter filterB = fixtureB.FilterData;
+
+            if (filterA.groupIndex == filterB.groupIndex && filterA.groupIndex != 0)
+            {
+                return filterA.groupIndex > 0;
+            }
+
+            bool collide = (filterA.maskBits & filterB.categoryBits) != 0 && (filterA.categoryBits & filterB.maskBits) != 0;
+            return collide;
+        }
+    }
 }
