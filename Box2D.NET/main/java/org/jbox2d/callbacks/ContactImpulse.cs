630d997e452cde8a66322a3ecd15f17a74cbbfb6
Sergey Gerasimov <gerich.home@gmail.com>
2012-01-25 10:06:11 +0400

    ContactImpulse.cs reworked

--- Box2D.NET/main/java/org/jbox2d/callbacks/ContactImpulse.cs
+++ Box2D.NET/main/java/org/jbox2d/callbacks/ContactImpulse.cs
@@ -1,55 +1,45 @@
-/// <summary>****************************************************************************
-/// Copyright (c) 2011, Daniel Murphy
-/// All rights reserved.
-/// 
-/// Redistribution and use in source and binary forms, with or without modification,
-/// are permitted provided that the following conditions are met:
-/// * Redistributions of source code must retain the above copyright notice,
-/// this list of conditions and the following disclaimer.
-/// * Redistributions in binary form must reproduce the above copyright notice,
-/// this list of conditions and the following disclaimer in the documentation
-/// and/or other materials provided with the distribution.
-/// 
-/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-/// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-/// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-/// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-/// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-/// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-/// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-/// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-/// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-/// POSSIBILITY OF SUCH DAMAGE.
-/// ****************************************************************************
-/// </summary>
-/// <summary> Created at 3:43:53 AM Jul 7, 2010</summary>
+// ****************************************************************************
+// Copyright (c) 2011, Daniel Murphy
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// ****************************************************************************
+
+// Created at 3:43:53 AM Jul 7, 2010
+
 using System;
 using Settings = org.jbox2d.common.Settings;
+
 namespace org.jbox2d.callbacks
 {
-	
-	/// <summary> Contact impulses for reporting. Impulses are used instead of forces because sub-step forces may
-	/// approach infinity for rigid body collisions. These match up one-to-one with the contact points in
-	/// b2Manifold.
-	/// 
-	/// </summary>
-	/// <author>  Daniel Murphy
-	/// </author>
-	public class ContactImpulse
-	{
-		public ContactImpulse()
-		{
-			InitBlock();
-		}
-		private void  InitBlock()
-		{
-			normalImpulses = new float[Settings.maxManifoldPoints];
-			tangentImpulses = new float[Settings.maxManifoldPoints];
-		}
-		//UPGRADE_NOTE: The initialization of  'normalImpulses' was moved to method 'InitBlock'. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1005'"
-		public float[] normalImpulses;
-		//UPGRADE_NOTE: The initialization of  'tangentImpulses' was moved to method 'InitBlock'. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1005'"
-		public float[] tangentImpulses;
-		public int count;
-	}
+
+    /// <summary>
+    /// Contact impulses for reporting. Impulses are used instead of forces because sub-step forces may
+    /// approach infinity for rigid body collisions. These match up one-to-one with the contact points in
+    /// b2Manifold.
+    /// </summary>
+    /// <author>Daniel Murphy</author>
+    public class ContactImpulse
+    {
+        public float[] normalImpulses = new float[Settings.maxManifoldPoints];
+        public float[] tangentImpulses = new float[Settings.maxManifoldPoints];
+        public int count;
+    }
 }
