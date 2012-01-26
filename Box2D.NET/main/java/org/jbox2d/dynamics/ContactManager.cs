277ed9ca752bdec45e3429111530c7c006cc9179
Sergey Gerasimov <gerich.home@gmail.com>
2012-01-26 14:38:12 +0400

    ContactManager.cs reworked

--- Box2D.NET/main/java/org/jbox2d/dynamics/ContactManager.cs
+++ Box2D.NET/main/java/org/jbox2d/dynamics/ContactManager.cs
@@ -29,299 +29,297 @@ using PairCallback = org.jbox2d.callbacks.PairCallback;
 using BroadPhase = org.jbox2d.collision.broadphase.BroadPhase;
 using Contact = org.jbox2d.dynamics.contacts.Contact;
 using ContactEdge = org.jbox2d.dynamics.contacts.ContactEdge;
+
 namespace org.jbox2d.dynamics
 {
-	
-	/// <summary> Delegate of World.
-	/// 
-	/// </summary>
-	/// <author>  Daniel Murphy
-	/// </author>
-	public class ContactManager : PairCallback
-	{
-		
-		public BroadPhase m_broadPhase;
-		public Contact m_contactList;
-		public int m_contactCount;
-		public ContactFilter m_contactFilter;
-		public ContactListener m_contactListener;
-		
-		//UPGRADE_NOTE: Final was removed from the declaration of 'pool '. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1003'"
-		private World pool;
-		
-		public ContactManager(World argPool)
-		{
-			m_contactList = null;
-			m_contactCount = 0;
-			m_contactFilter = new ContactFilter();
-			m_contactListener = null;
-			m_broadPhase = new BroadPhase();
-			pool = argPool;
-		}
-		
-		/// <summary> Broad-phase callback.
-		/// 
-		/// </summary>
-		/// <param name="proxyUserDataA">
-		/// </param>
-		/// <param name="proxyUserDataB">
-		/// </param>
-		public virtual void  addPair(System.Object proxyUserDataA, System.Object proxyUserDataB)
-		{
-			FixtureProxy proxyA = (FixtureProxy) proxyUserDataA;
-			FixtureProxy proxyB = (FixtureProxy) proxyUserDataB;
-			
-			Fixture fixtureA = proxyA.fixture;
-			Fixture fixtureB = proxyB.fixture;
-			
-			int indexA = proxyA.childIndex;
-			int indexB = proxyB.childIndex;
-			
-			Body bodyA = fixtureA.Body;
-			Body bodyB = fixtureB.Body;
-			
-			// Are the fixtures on the same body?
-			if (bodyA == bodyB)
-			{
-				return ;
-			}
-			
-			// TODO_ERIN use a hash table to remove a potential bottleneck when both
-			// bodies have a lot of contacts.
-			// Does a contact already exist?
-			ContactEdge edge = bodyB.ContactList;
-			while (edge != null)
-			{
-				if (edge.other == bodyA)
-				{
-					Fixture fA = edge.contact.FixtureA;
-					Fixture fB = edge.contact.FixtureB;
-					int iA = edge.contact.ChildIndexA;
-					int iB = edge.contact.ChildIndexB;
-					
-					if (fA == fixtureA && iA == indexA && fB == fixtureB && iB == indexB)
-					{
-						// A contact already exists.
-						return ;
-					}
-					
-					if (fA == fixtureB && iA == indexB && fB == fixtureA && iB == indexA)
-					{
-						// A contact already exists.
-						return ;
-					}
-				}
-				
-				edge = edge.next;
-			}
-			
-			// Does a joint override collision? is at least one body dynamic?
-			if (bodyB.shouldCollide(bodyA) == false)
-			{
-				return ;
-			}
-			
-			// Check user filtering.
-			if (m_contactFilter != null && m_contactFilter.shouldCollide(fixtureA, fixtureB) == false)
-			{
-				return ;
-			}
-			
-			// Call the factory.
-			Contact c = pool.popContact(fixtureA, indexA, fixtureB, indexB);
-			if (c == null)
-			{
-				return ;
-			}
-			
-			// Contact creation may swap fixtures.
-			fixtureA = c.FixtureA;
-			fixtureB = c.FixtureB;
-			indexA = c.ChildIndexA;
-			indexB = c.ChildIndexB;
-			bodyA = fixtureA.Body;
-			bodyB = fixtureB.Body;
-			
-			// Insert into the world.
-			c.m_prev = null;
-			c.m_next = m_contactList;
-			if (m_contactList != null)
-			{
-				m_contactList.m_prev = c;
-			}
-			m_contactList = c;
-			
-			// Connect to island graph.
-			
-			// Connect to body A
-			c.m_nodeA.contact = c;
-			c.m_nodeA.other = bodyB;
-			
-			c.m_nodeA.prev = null;
-			c.m_nodeA.next = bodyA.m_contactList;
-			if (bodyA.m_contactList != null)
-			{
-				bodyA.m_contactList.prev = c.m_nodeA;
-			}
-			bodyA.m_contactList = c.m_nodeA;
-			
-			// Connect to body B
-			c.m_nodeB.contact = c;
-			c.m_nodeB.other = bodyA;
-			
-			c.m_nodeB.prev = null;
-			c.m_nodeB.next = bodyB.m_contactList;
-			if (bodyB.m_contactList != null)
-			{
-				bodyB.m_contactList.prev = c.m_nodeB;
-			}
-			bodyB.m_contactList = c.m_nodeB;
-			
-			// wake up the bodies
-			bodyA.Awake = true;
-			bodyB.Awake = true;
-			
-			++m_contactCount;
-		}
-		
-		public virtual void  findNewContacts()
-		{
-			m_broadPhase.updatePairs(this);
-		}
-		
-		public virtual void  destroy(Contact c)
-		{
-			Fixture fixtureA = c.FixtureA;
-			Fixture fixtureB = c.FixtureB;
-			Body bodyA = fixtureA.Body;
-			Body bodyB = fixtureB.Body;
-			
-			if (m_contactListener != null && c.Touching)
-			{
-				m_contactListener.endContact(c);
-			}
-			
-			// Remove from the world.
-			if (c.m_prev != null)
-			{
-				c.m_prev.m_next = c.m_next;
-			}
-			
-			if (c.m_next != null)
-			{
-				c.m_next.m_prev = c.m_prev;
-			}
-			
-			if (c == m_contactList)
-			{
-				m_contactList = c.m_next;
-			}
-			
-			// Remove from body 1
-			if (c.m_nodeA.prev != null)
-			{
-				c.m_nodeA.prev.next = c.m_nodeA.next;
-			}
-			
-			if (c.m_nodeA.next != null)
-			{
-				c.m_nodeA.next.prev = c.m_nodeA.prev;
-			}
-			
-			if (c.m_nodeA == bodyA.m_contactList)
-			{
-				bodyA.m_contactList = c.m_nodeA.next;
-			}
-			
-			// Remove from body 2
-			if (c.m_nodeB.prev != null)
-			{
-				c.m_nodeB.prev.next = c.m_nodeB.next;
-			}
-			
-			if (c.m_nodeB.next != null)
-			{
-				c.m_nodeB.next.prev = c.m_nodeB.prev;
-			}
-			
-			if (c.m_nodeB == bodyB.m_contactList)
-			{
-				bodyB.m_contactList = c.m_nodeB.next;
-			}
-			
-			// Call the factory.
-			pool.pushContact(c);
-			--m_contactCount;
-		}
-		
-		/// <summary> This is the top level collision call for the time step. Here all the narrow phase collision is
-		/// processed for the world contact list.
-		/// </summary>
-		public virtual void  collide()
-		{
-			// Update awake contacts.
-			Contact c = m_contactList;
-			while (c != null)
-			{
-				Fixture fixtureA = c.FixtureA;
-				Fixture fixtureB = c.FixtureB;
-				int indexA = c.ChildIndexA;
-				int indexB = c.ChildIndexB;
-				Body bodyA = fixtureA.Body;
-				Body bodyB = fixtureB.Body;
-				
-				// is this contact flagged for filtering?
-				if ((c.m_flags & Contact.FILTER_FLAG) == Contact.FILTER_FLAG)
-				{
-					// Should these bodies collide?
-					if (bodyB.shouldCollide(bodyA) == false)
-					{
-						Contact cNuke = c;
-						c = cNuke.Next;
-						destroy(cNuke);
-						continue;
-					}
-					
-					// Check user filtering.
-					if (m_contactFilter != null && m_contactFilter.shouldCollide(fixtureA, fixtureB) == false)
-					{
-						Contact cNuke = c;
-						c = cNuke.Next;
-						destroy(cNuke);
-						continue;
-					}
-					
-					// Clear the filtering flag.
-					c.m_flags &= ~ Contact.FILTER_FLAG;
-				}
-				
-				bool activeA = bodyA.Awake && bodyA.m_type != BodyType.STATIC;
-				bool activeB = bodyB.Awake && bodyB.m_type != BodyType.STATIC;
-				
-				// At least one body must be awake and it must be dynamic or kinematic.
-				if (activeA == false && activeB == false)
-				{
-					c = c.Next;
-					continue;
-				}
-				
-				int proxyIdA = fixtureA.m_proxies[indexA].proxyId;
-				int proxyIdB = fixtureB.m_proxies[indexB].proxyId;
-				bool overlap = m_broadPhase.testOverlap(proxyIdA, proxyIdB);
-				
-				// Here we destroy contacts that cease to overlap in the broad-phase.
-				if (overlap == false)
-				{
-					Contact cNuke = c;
-					c = cNuke.Next;
-					destroy(cNuke);
-					continue;
-				}
-				
-				// The contact persists.
-				c.update(m_contactListener);
-				c = c.Next;
-			}
-		}
-	}
+
+    /// <summary>
+    /// Delegate of World.
+    /// </summary>
+    /// <author>Daniel Murphy</author>
+    public class ContactManager : PairCallback
+    {
+
+        public BroadPhase m_broadPhase;
+        public Contact m_contactList;
+        public int m_contactCount;
+        public ContactFilter m_contactFilter;
+        public ContactListener m_contactListener;
+
+        private readonly World pool;
+
+        public ContactManager(World argPool)
+        {
+            m_contactList = null;
+            m_contactCount = 0;
+            m_contactFilter = new ContactFilter();
+            m_contactListener = null;
+            m_broadPhase = new BroadPhase();
+            pool = argPool;
+        }
+
+        /// <summary>
+        /// Broad-phase callback.
+        /// </summary>
+        /// <param name="proxyUserDataA"></param>
+        /// <param name="proxyUserDataB"></param>
+        public virtual void addPair(object proxyUserDataA, object proxyUserDataB)
+        {
+            FixtureProxy proxyA = (FixtureProxy)proxyUserDataA;
+            FixtureProxy proxyB = (FixtureProxy)proxyUserDataB;
+
+            Fixture fixtureA = proxyA.fixture;
+            Fixture fixtureB = proxyB.fixture;
+
+            int indexA = proxyA.childIndex;
+            int indexB = proxyB.childIndex;
+
+            Body bodyA = fixtureA.Body;
+            Body bodyB = fixtureB.Body;
+
+            // Are the fixtures on the same body?
+            if (bodyA == bodyB)
+            {
+                return;
+            }
+
+            // TODO_ERIN use a hash table to remove a potential bottleneck when both
+            // bodies have a lot of contacts.
+            // Does a contact already exist?
+            ContactEdge edge = bodyB.ContactList;
+            while (edge != null)
+            {
+                if (edge.other == bodyA)
+                {
+                    Fixture fA = edge.contact.FixtureA;
+                    Fixture fB = edge.contact.FixtureB;
+                    int iA = edge.contact.ChildIndexA;
+                    int iB = edge.contact.ChildIndexB;
+
+                    if (fA == fixtureA && iA == indexA && fB == fixtureB && iB == indexB)
+                    {
+                        // A contact already exists.
+                        return;
+                    }
+
+                    if (fA == fixtureB && iA == indexB && fB == fixtureA && iB == indexA)
+                    {
+                        // A contact already exists.
+                        return;
+                    }
+                }
+
+                edge = edge.next;
+            }
+
+            // Does a joint override collision? is at least one body dynamic?
+            if (bodyB.shouldCollide(bodyA) == false)
+            {
+                return;
+            }
+
+            // Check user filtering.
+            if (m_contactFilter != null && m_contactFilter.shouldCollide(fixtureA, fixtureB) == false)
+            {
+                return;
+            }
+
+            // Call the factory.
+            Contact c = pool.popContact(fixtureA, indexA, fixtureB, indexB);
+            if (c == null)
+            {
+                return;
+            }
+
+            // Contact creation may swap fixtures.
+            fixtureA = c.FixtureA;
+            fixtureB = c.FixtureB;
+            indexA = c.ChildIndexA;
+            indexB = c.ChildIndexB;
+            bodyA = fixtureA.Body;
+            bodyB = fixtureB.Body;
+
+            // Insert into the world.
+            c.m_prev = null;
+            c.m_next = m_contactList;
+            if (m_contactList != null)
+            {
+                m_contactList.m_prev = c;
+            }
+            m_contactList = c;
+
+            // Connect to island graph.
+
+            // Connect to body A
+            c.m_nodeA.contact = c;
+            c.m_nodeA.other = bodyB;
+
+            c.m_nodeA.prev = null;
+            c.m_nodeA.next = bodyA.m_contactList;
+            if (bodyA.m_contactList != null)
+            {
+                bodyA.m_contactList.prev = c.m_nodeA;
+            }
+            bodyA.m_contactList = c.m_nodeA;
+
+            // Connect to body B
+            c.m_nodeB.contact = c;
+            c.m_nodeB.other = bodyA;
+
+            c.m_nodeB.prev = null;
+            c.m_nodeB.next = bodyB.m_contactList;
+            if (bodyB.m_contactList != null)
+            {
+                bodyB.m_contactList.prev = c.m_nodeB;
+            }
+            bodyB.m_contactList = c.m_nodeB;
+
+            // wake up the bodies
+            bodyA.Awake = true;
+            bodyB.Awake = true;
+
+            ++m_contactCount;
+        }
+
+        public virtual void findNewContacts()
+        {
+            m_broadPhase.updatePairs(this);
+        }
+
+        public virtual void destroy(Contact c)
+        {
+            Fixture fixtureA = c.FixtureA;
+            Fixture fixtureB = c.FixtureB;
+            Body bodyA = fixtureA.Body;
+            Body bodyB = fixtureB.Body;
+
+            if (m_contactListener != null && c.Touching)
+            {
+                m_contactListener.endContact(c);
+            }
+
+            // Remove from the world.
+            if (c.m_prev != null)
+            {
+                c.m_prev.m_next = c.m_next;
+            }
+
+            if (c.m_next != null)
+            {
+                c.m_next.m_prev = c.m_prev;
+            }
+
+            if (c == m_contactList)
+            {
+                m_contactList = c.m_next;
+            }
+
+            // Remove from body 1
+            if (c.m_nodeA.prev != null)
+            {
+                c.m_nodeA.prev.next = c.m_nodeA.next;
+            }
+
+            if (c.m_nodeA.next != null)
+            {
+                c.m_nodeA.next.prev = c.m_nodeA.prev;
+            }
+
+            if (c.m_nodeA == bodyA.m_contactList)
+            {
+                bodyA.m_contactList = c.m_nodeA.next;
+            }
+
+            // Remove from body 2
+            if (c.m_nodeB.prev != null)
+            {
+                c.m_nodeB.prev.next = c.m_nodeB.next;
+            }
+
+            if (c.m_nodeB.next != null)
+            {
+                c.m_nodeB.next.prev = c.m_nodeB.prev;
+            }
+
+            if (c.m_nodeB == bodyB.m_contactList)
+            {
+                bodyB.m_contactList = c.m_nodeB.next;
+            }
+
+            // Call the factory.
+            pool.pushContact(c);
+            --m_contactCount;
+        }
+
+        /// <summary>
+        /// This is the top level collision call for the time step. Here all the narrow phase collision is
+        /// processed for the world contact list.
+        /// </summary>
+        public virtual void collide()
+        {
+            // Update awake contacts.
+            Contact c = m_contactList;
+            while (c != null)
+            {
+                Fixture fixtureA = c.FixtureA;
+                Fixture fixtureB = c.FixtureB;
+                int indexA = c.ChildIndexA;
+                int indexB = c.ChildIndexB;
+                Body bodyA = fixtureA.Body;
+                Body bodyB = fixtureB.Body;
+
+                // is this contact flagged for filtering?
+                if ((c.m_flags & Contact.FILTER_FLAG) == Contact.FILTER_FLAG)
+                {
+                    // Should these bodies collide?
+                    if (bodyB.shouldCollide(bodyA) == false)
+                    {
+                        Contact cNuke = c;
+                        c = cNuke.Next;
+                        destroy(cNuke);
+                        continue;
+                    }
+
+                    // Check user filtering.
+                    if (m_contactFilter != null && m_contactFilter.shouldCollide(fixtureA, fixtureB) == false)
+                    {
+                        Contact cNuke = c;
+                        c = cNuke.Next;
+                        destroy(cNuke);
+                        continue;
+                    }
+
+                    // Clear the filtering flag.
+                    c.m_flags &= ~Contact.FILTER_FLAG;
+                }
+
+                bool activeA = bodyA.Awake && bodyA.m_type != BodyType.STATIC;
+                bool activeB = bodyB.Awake && bodyB.m_type != BodyType.STATIC;
+
+                // At least one body must be awake and it must be dynamic or kinematic.
+                if (activeA == false && activeB == false)
+                {
+                    c = c.Next;
+                    continue;
+                }
+
+                int proxyIdA = fixtureA.m_proxies[indexA].proxyId;
+                int proxyIdB = fixtureB.m_proxies[indexB].proxyId;
+                bool overlap = m_broadPhase.testOverlap(proxyIdA, proxyIdB);
+
+                // Here we destroy contacts that cease to overlap in the broad-phase.
+                if (overlap == false)
+                {
+                    Contact cNuke = c;
+                    c = cNuke.Next;
+                    destroy(cNuke);
+                    continue;
+                }
+
+                // The contact persists.
+                c.update(m_contactListener);
+                c = c.Next;
+            }
+        }
+    }
 }
