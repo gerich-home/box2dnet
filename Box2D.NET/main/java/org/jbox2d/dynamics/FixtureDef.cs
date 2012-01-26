68d8842db6dc586ffa85d325f675d6e9fb73152f
Sergey Gerasimov <gerich.home@gmail.com>
2012-01-26 09:48:27 +0400

    FixtureDef.cs reworked

--- Box2D.NET/main/java/org/jbox2d/dynamics/FixtureDef.cs
+++ Box2D.NET/main/java/org/jbox2d/dynamics/FixtureDef.cs
@@ -24,52 +24,63 @@
 
 using System;
 using Shape = org.jbox2d.collision.shapes.Shape;
+
 namespace org.jbox2d.dynamics
 {
-	
-	// updated to rev 100
-	/// <summary> A fixture definition is used to create a fixture. This class defines an
-	/// abstract fixture definition. You can reuse fixture definitions safely.
-	/// 
-	/// </summary>
-	/// <author>  daniel
-	/// </author>
-	public class FixtureDef
-	{
-		/// <summary> The shape, this must be set. The shape will be cloned, so you
-		/// can create the shape on the stack.
-		/// </summary>
-		public Shape shape = null;
-		
-		/// <summary> Use this to store application specific fixture data.</summary>
-		public System.Object userData;
-		
-		/// <summary> The friction coefficient, usually in the range [0,1].</summary>
-		public float friction;
-		
-		/// <summary> The restitution (elasticity) usually in the range [0,1].</summary>
-		public float restitution;
-		
-		/// <summary> The density, usually in kg/m^2</summary>
-		public float density;
-		
-		/// <summary> A sensor shape collects contact information but never generates a collision
-		/// response.
-		/// </summary>
-		public bool isSensor;
-		
-		/// <summary> Contact filtering data;</summary>
-		public Filter filter;
-		
-		public FixtureDef()
-		{
-			shape = null;
-			userData = null;
-			friction = 0.2f;
-			restitution = 0f;
-			density = 0f;
-			filter = new Filter();
-			isSensor = false;
-		}
-	}
+    // updated to rev 100
+    /// <summary>
+    /// A fixture definition is used to create a fixture. This class defines an
+    /// abstract fixture definition. You can reuse fixture definitions safely.
+    /// </summary>
+    /// <author>daniel</author>
+    public class FixtureDef
+    {
+        /// <summary>
+        /// The shape, this must be set. The shape will be cloned, so you
+        /// can create the shape on the stack.
+        /// </summary>
+        public Shape shape = null;
+
+        /// <summary>
+        /// Use this to store application specific fixture data.
+        /// </summary>
+        public object userData;
+
+        /// <summary>
+        /// The friction coefficient, usually in the range [0,1].
+        /// </summary>
+        public float friction;
+
+        /// <summary>
+        /// The restitution (elasticity) usually in the range [0,1].
+        /// </summary>
+        public float restitution;
+
+        /// <summary>
+        /// The density, usually in kg/m^2
+        /// </summary>
+        public float density;
+
+        /// <summary>
+        /// A sensor shape collects contact information but never generates a collision
+        /// response.
+        /// </summary>
+        public bool isSensor;
+
+        /// <summary>
+        /// Contact filtering data
+        /// </summary>
+        public Filter filter;
+
+        public FixtureDef()
+        {
+            shape = null;
+            userData = null;
+            friction = 0.2f;
+            restitution = 0f;
+            density = 0f;
+            filter = new Filter();
+            isSensor = false;
+        }
+    }
 }
