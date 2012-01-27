7875cb9840ec36e35d706175985936403f0120f7
Sergey Gerasimov <gerich.home@gmail.com>
2012-01-27 15:15:20 +0400

    PolygonAndCircleContact.cs reworked

--- Box2D.NET/main/java/org/jbox2d/dynamics/contacts/PolygonAndCircleContact.cs
+++ Box2D.NET/main/java/org/jbox2d/dynamics/contacts/PolygonAndCircleContact.cs
@@ -26,32 +26,32 @@ using System;
 using Manifold = org.jbox2d.collision.Manifold;
 using CircleShape = org.jbox2d.collision.shapes.CircleShape;
 using PolygonShape = org.jbox2d.collision.shapes.PolygonShape;
-//UPGRADE_TODO: The type 'org.jbox2d.collision.shapes.ShapeType' could not be found. If it was not included in the conversion, there may be compiler issues. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1262'"
 using ShapeType = org.jbox2d.collision.shapes.ShapeType;
 using Transform = org.jbox2d.common.Transform;
 using Fixture = org.jbox2d.dynamics.Fixture;
 using IWorldPool = org.jbox2d.pooling.IWorldPool;
+using System.Diagnostics;
+
 namespace org.jbox2d.dynamics.contacts
 {
-	
-	public class PolygonAndCircleContact:Contact
-	{
-		
-		public PolygonAndCircleContact(IWorldPool argPool):base(argPool)
-		{
-		}
-		
-		public virtual void  init(Fixture fixtureA, Fixture fixtureB)
-		{
-			base.init(fixtureA, 0, fixtureB, 0);
-			assert(m_fixtureA.Type == ShapeType.POLYGON);
-			assert(m_fixtureB.Type == ShapeType.CIRCLE);
-		}
-		//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-		Override
-		public override void  evaluate(Manifold manifold, Transform xfA, Transform xfB)
-		{
-			pool.getCollision().collidePolygonAndCircle(m_manifold, (PolygonShape) m_fixtureA.Shape, xfA, (CircleShape) m_fixtureB.Shape, xfB);
-		}
-	}
+
+    public class PolygonAndCircleContact : Contact
+    {
+        public PolygonAndCircleContact(IWorldPool argPool)
+            : base(argPool)
+        {
+        }
+
+        public virtual void init(Fixture fixtureA, Fixture fixtureB)
+        {
+            base.init(fixtureA, 0, fixtureB, 0);
+            Debug.Assert(m_fixtureA.Type == ShapeType.POLYGON);
+            Debug.Assert(m_fixtureB.Type == ShapeType.CIRCLE);
+        }
+
+        public override void evaluate(Manifold manifold, Transform xfA, Transform xfB)
+        {
+            pool.getCollision().collidePolygonAndCircle(m_manifold, (PolygonShape)m_fixtureA.Shape, xfA, (CircleShape)m_fixtureB.Shape, xfB);
+        }
+    }
 }
