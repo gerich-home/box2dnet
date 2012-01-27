dc788550d2d1aa3a3bc4366ef81af7ac624637e0
Sergey Gerasimov <gerich.home@gmail.com>
2012-01-27 13:20:58 +0400

    IOrderedStack.cs reworked

--- Box2D.NET/main/java/org/jbox2d/pooling/IOrderedStack.cs
+++ Box2D.NET/main/java/org/jbox2d/pooling/IOrderedStack.cs
@@ -23,50 +23,35 @@
 // ****************************************************************************
 
 using System;
+
 namespace org.jbox2d.pooling
 {
-	
-	/// <summary> This stack assumes that when you push 'n' items back,
+
+	/// <summary>
+	/// This stack assumes that when you push 'n' items back,
 	/// you're pushing back the last 'n' items popped.
 	/// </summary>
-	/// <author>  Daniel
-	/// 
-	/// </author>
-	/// <param name="<E>">
-	/// </param>
-	//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-	public
-	//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-	interface IOrderedStack < E >
-	//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
+	/// <author>Daniel</author>
+	/// <typeparam name="E"></typeparam>
+	public interface IOrderedStack<E>
 	{
-	
-	/// <summary> Returns the next object in the pool</summary>
-	/// <returns>
-	/// </returns>
-	//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-	public E pop();
-	
-	/// <summary> Returns the next 'argNum' objects in the pool
-	/// in an array
-	/// </summary>
-	/// <param name="argNum">
-	/// </param>
-	/// <returns> an array containing the next pool objects in
-	/// items 0-argNum.  Array length and uniqueness not
-	/// guaranteed.
-	/// </returns>
-	//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-	public E [] pop(int argNum);
-	
-	/// <summary> Tells the stack to take back the last 'argNum' items</summary>
-	/// <param name="argNum">
-	/// </param>
-	//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-	public
-	//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-	void push(int argNum);
-	
-	//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
+		/// <summary>
+		/// Returns the next object in the pool
+		/// </summary>
+		/// <returns></returns>
+		public E pop();
+
+		/// <summary>
+		/// Returns the next 'argNum' objects in the pool in an array
+		/// </summary>
+		/// <param name="argNum"></param>
+		/// <returns>an array containing the next pool objects in items 0-argNum. Array length and uniqueness not guaranteed.</returns>
+		public E[] pop(int argNum);
+
+		/// <summary>
+		/// Tells the stack to take back the last 'argNum' items
+		/// </summary>
+		/// <param name="argNum"></param>
+		public void push(int argNum);
 	}
 }
