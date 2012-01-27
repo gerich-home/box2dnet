78b83600d56e00afe5f7e7d162747152defcea8f
Sergey Gerasimov <gerich.home@gmail.com>
2012-01-27 13:49:48 +0400

    CircleStack.cs reworked

--- Box2D.NET/main/java/org/jbox2d/pooling/normal/CircleStack.cs
+++ Box2D.NET/main/java/org/jbox2d/pooling/normal/CircleStack.cs
@@ -23,102 +23,93 @@
 // ****************************************************************************
 
 using System;
-//UPGRADE_TODO: The type 'org.jbox2d.pooling.IOrderedStack' could not be found. If it was not included in the conversion, there may be compiler issues. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1262'"
-using IOrderedStack = org.jbox2d.pooling.IOrderedStack;
+using org.jbox2d.pooling;
+using System.Diagnostics;
 //UPGRADE_TODO: The type 'org.slf4j.Logger' could not be found. If it was not included in the conversion, there may be compiler issues. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1262'"
-using Logger = org.slf4j.Logger;
+//using Logger = org.slf4j.Logger;
 //UPGRADE_TODO: The type 'org.slf4j.LoggerFactory' could not be found. If it was not included in the conversion, there may be compiler issues. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1262'"
-using LoggerFactory = org.slf4j.LoggerFactory;
+//using LoggerFactory = org.slf4j.LoggerFactory;
+
 namespace org.jbox2d.pooling.normal
 {
-	
-	public class CircleStack
-	{
-		public CircleStack()
-		{
-			InitBlock();
-		}
-		private void  InitBlock()
-		{
-			size = argStackSize;
-			pool = (E[]) Array.newInstance(argClass, argStackSize);
-			for (int i = 0; i < argStackSize; i++)
-			{
-				try
-				{
-					pool[i] = argClass.newInstance();
-				}
-				//UPGRADE_NOTE: Exception 'java.lang.InstantiationException' was converted to 'System.Exception' which has different behavior. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1100'"
-				catch (System.Exception e)
-				{
-					log.error("Error creating pooled object " + argClass.getSimpleName(), e);
-					//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-					assert(false): Error creating pooled object  + argClass.getCanonicalName();
-				}
-				catch (System.UnauthorizedAccessException e)
-				{
-					log.error("Error creating pooled object " + argClass.getSimpleName(), e);
-					//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-					assert(false): Error creating pooled object  + argClass.getCanonicalName();
-				}
-			}
-			index = 0;
-			container = (E[]) Array.newInstance(argClass, argContainerSize);
-		}
-		//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-		< E > implements IOrderedStack < E >
-		//UPGRADE_NOTE: Final was removed from the declaration of 'log '. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1003'"
-		//UPGRADE_NOTE: The initialization of  'log' was moved to static method 'org.jbox2d.pooling.normal.CircleStack'. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1005'"
-		private static readonly Logger log;
-		
-		//UPGRADE_NOTE: Final was removed from the declaration of 'pool '. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1003'"
-		private E[] pool;
-		private int index;
-		//UPGRADE_NOTE: Final was removed from the declaration of 'size '. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1003'"
-		private int size;
-		//UPGRADE_NOTE: Final was removed from the declaration of 'container '. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1003'"
-		private E[] container;
-		//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-		SuppressWarnings(unchecked)
-		//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-		public CircleStack(Class < E > argClass, int argStackSize, int argContainerSize)
-		
-		public E pop()
-		{
-			index++;
-			if (index >= size)
-			{
-				index = 0;
-			}
-			return pool[index];
-		}
-		
-		public E[] pop(int argNum)
-		{
-			//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-			assert(argNum <= container.length): Container array is too small;
-			if (index + argNum < size)
-			{
-				Array.Copy(pool, index, container, 0, argNum);
-				index += argNum;
-			}
-			else
-			{
-				int overlap = (index + argNum) - size;
-				Array.Copy(pool, index, container, 0, argNum - overlap);
-				Array.Copy(pool, 0, container, argNum - overlap, overlap);
-				index = overlap;
-			}
-			return container;
-		}
-		//UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
-		Override
-		public virtual void  push(int argNum)
-		{
-		}
-		static CircleStack()
-		{
-			log = LoggerFactory.getLogger(typeof(CircleStack));
-		}
-	}
+
+    public class CircleStack<E> : IOrderedStack<E>
+    {
+        //UPGRADE_TODO: there is no logger class
+        //private static readonly Logger log;
+        //static CircleStack()
+        //{
+        //	log = LoggerFactory.getLogger(typeof(CircleStack));
+        //}
+
+        private readonly E[] pool;
+        private int index;
+        private readonly int size;
+        private readonly E[] container;
+
+        //UPGRADE_TODO: check if we can remove first argument and use
+        //              where E : new() constraint in couple with
+        //              pool = new E[argStackSize];
+        //              and
+        //              pool[i] = new E();
+        public CircleStack(Type argClass, int argStackSize, int argContainerSize)
+        {
+            size = argStackSize;
+            pool = (E[])Array.CreateInstance(argClass, argStackSize);
+
+            for (int i = 0; i < argStackSize; i++)
+            {
+                try
+                {
+                    pool[i] = (E)argClass.GetConstructor(Type.EmptyTypes).Invoke(new object[0]);
+                }
+                catch (Exception e)
+                {
+                    //log.error("Error creating pooled object " + argClass.getSimpleName(), e);
+                    //UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
+                    Debug.Assert(false); // Error creating pooled object  + argClass.getCanonicalName();
+                }
+                /*catch (System.UnauthorizedAccessException e)
+                {
+                    log.error("Error creating pooled object " + argClass.getSimpleName(), e);
+                    //UPGRADE_ISSUE: The following fragment of code could not be parsed and was not converted. "ms-help://MS.VSCC.v80/dv_commoner/local/redirect.htm?index='!DefaultContextWindowIndex'&keyword='jlca1156'"
+                    assert(false): Error creating pooled object  + argClass.getCanonicalName();
+                }*/
+            }
+            index = 0;
+            container = (E[])Array.CreateInstance(argClass, argContainerSize);
+        }
+
+        public E pop()
+        {
+            index++;
+            if (index >= size)
+            {
+                index = 0;
+            }
+            return pool[index];
+        }
+
+        public E[] pop(int argNum)
+        {
+            Debug.Assert(argNum <= container.Length); //Container array is too small
+            if (index + argNum < size)
+            {
+                Array.Copy(pool, index, container, 0, argNum);
+                index += argNum;
+            }
+            else
+            {
+                int overlap = (index + argNum) - size;
+                Array.Copy(pool, index, container, 0, argNum - overlap);
+                Array.Copy(pool, 0, container, argNum - overlap, overlap);
+                index = overlap;
+            }
+            return container;
+        }
+
+        public virtual void push(int argNum)
+        {
+        }
+    }
 }
